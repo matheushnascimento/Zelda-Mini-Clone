f71336a2a5bd5e6a3d2197cd02986b5a3f817a61
matheushnascimento <matheus.mhddn@gmail.com>
2024-07-26 16:36:02 -0300

    :rocket:create player controller logic

--- src/Game.java
+++ src/Game.java
@@ -1,26 +1,34 @@
 
+//imports
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferStrategy;
 
 import javax.swing.JFrame;
 
-public class Game extends Canvas implements Runnable{
+public class Game extends Canvas implements Runnable, KeyListener {
   public static int WIDTH = 480, HEIGHT = 480;
-  
+  public Player player;
+
   public Game() {
+    this.addKeyListener(this);
     this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+
+    player = new Player(0, 0);
   }
 
-  public static void tick() {
+  public void tick() {
+    player.tick();
   }
 
   public void render() {
     BufferStrategy bs = this.getBufferStrategy();
 
-    if(bs == null) {
+    if (bs == null) {
       this.createBufferStrategy(3);
       return;
     }
@@ -30,18 +38,18 @@ public class Game extends Canvas implements Runnable{
     g.setColor(Color.black);
     g.fillRect(0, 0, WIDTH, HEIGHT);
 
-    g.setColor(Color.red);
-    g.fillRect(0, 0, 50, 50);
-    
+    player.render(g);
+
     bs.show();
   }
-  public static void main(String[] args){
+
+  public static void main(String[] args) {
     Game game = new Game();
     JFrame frame = new JFrame("Mini Zelda");
     frame.add(game);
     frame.setTitle("Mini Zelda");
     frame.pack();
-    
+
     frame.setLocationRelativeTo(null);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -49,19 +57,54 @@ public class Game extends Canvas implements Runnable{
 
     new Thread(game).start();
   }
- 
+
   @Override
   public void run() {
     while (true) {
       tick();
       render();
       try {
-        Thread.sleep(1000/60);
+        Thread.sleep(1000 / 60);
       } catch (InterruptedException e) {
         e.printStackTrace();
       }
-      }
+    }
+  }
+
+  @Override
+  public void keyTyped(KeyEvent e) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
+  }
+
+  @Override
+  public void keyPressed(KeyEvent e) {
+    if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+      player.right = true;
+    } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+      player.left = true;
     }
 
-}
+    if (e.getKeyCode() == KeyEvent.VK_UP) {
+      player.up = true;
+    } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+      player.down = true;
+    }
+  }
 
+  @Override
+  public void keyReleased(KeyEvent e) {
+    if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+      player.right = false;
+    } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+      player.left = false;
+    }
+
+    if (e.getKeyCode() == KeyEvent.VK_UP) {
+      player.up = false;
+    } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+      player.down = false;
+    }
+  }
+
+}
